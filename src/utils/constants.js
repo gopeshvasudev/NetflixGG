d27915be2042b77d20d0e8e1587ab2f8035369c1
Gopesh018 <140505822+Gopesh018@users.noreply.github.com>
2024-11-15 10:48:04 +0530

    Securing token

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ const API_OPTIONS = {
   headers: {
     accept: "application/json",
     Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MmI1MjkwZTJhOWQxNjAyZTdmN2NiYWIyNjlkZGY0YSIsIm5iZiI6MTczMTI0MTUxNi41NDY5NDM0LCJzdWIiOiI2NmZlODhhMzE1OTJlZjFiYTk4NGRhYWMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.WCkpCPCNdR90fxWG88cE3ps-mOsYeAYiBooX86Xam6M",
+      `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
   },
 };
 
