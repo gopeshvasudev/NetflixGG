e0cea50a6ba29769396957a6b724ffe00e054e2d
Gopesh018 <140505822+Gopesh018@users.noreply.github.com>
2024-12-06 15:53:04 +0530

    Add favicon and did some fixex in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import LogoutConfirm from "./LogoutConfirm";
+import { RiMenu3Fill } from "react-icons/ri";
+import { IoMdClose } from "react-icons/io";
 
 const Header = () => {
   const [logoutConfirm, setLogoutConfirm] = useState(false);
+  const [isSmallDevice, setIsSmallDevice] = useState(false);
+
   const user = useSelector((store) => store.auth.user);
 
   const handleLogoutConfirm = () => {
     setLogoutConfirm(true);
   };
 
+  const toggleNavigators = () => {
+    setIsSmallDevice(!isSmallDevice);
+  };
+  
   return (
     <>
-      <header className="fixed w-full top-0 left-0 z-[100] overflow-hidden">
+      <header className="fixed w-full top-0 left-0 z-[100]">
         {logoutConfirm && <LogoutConfirm setLogoutConfirm={setLogoutConfirm} />}
         <nav className="relative navbar w-full h-[12vh] bg-gradient-to-b from-zinc-950 to-transparent text-white flex items-center justify-between px-2 sm:px-5">
           <div className="left flex items-center gap-10">
@@ -24,7 +32,7 @@ const Header = () => {
 
           <div className="right">
             <div className="flex items-center gap-3">
-              <div className="avatar w-[40px] h-[40px] border-2 border-white rounded-full overflow-hidden">
+              <div className="avatar w-[35px] h-[35px] border-2 border-white rounded-full overflow-hidden">
                 <img
                   src={user?.avatar}
                   className="w-full h-full object-cover invert-[100%]"
@@ -32,19 +40,31 @@ const Header = () => {
                 />
               </div>
 
-              <button
-                onClick={handleLogoutConfirm}
-                className="py-2 px-3 bg-red-600 text-white rounded-full font-semibold"
+              <div
+                className={`absolute top-[100%] md:static z-50 flex flex-col md:flex-row items-center gap-2 bg-black/50 md:bg-transparent p-5 md:p-0 rounded-[10px_0px_0px_10px] duration-300 ${
+                  !isSmallDevice ? "right-[-100%]" : "right-0"
+                }`}
               >
-                Logout
-              </button>
+                <button
+                  onClick={handleLogoutConfirm}
+                  className="w-full md:w-fit text-sm py-2 px-3 bg-red-600 text-white rounded-full font-semibold"
+                >
+                  Logout
+                </button>
 
-              <Link
-                to={"/search-ai"}
-                className="py-2 px-3 bg-red-600 text-white rounded-full font-semibold"
+                <Link
+                  to={"/search-ai"}
+                  className="w-full md:w-fit text-sm py-2 px-3 bg-red-600 text-white rounded-full font-semibold"
+                >
+                  Ask to AI
+                </Link>
+              </div>
+              <div
+                className="md:hidden text-3xl duration-300"
+                onClick={toggleNavigators}
               >
-                Search Ai
-              </Link>
+                {!isSmallDevice ? <RiMenu3Fill /> : <IoMdClose />}
+              </div>
             </div>
           </div>
         </nav>
--- src/pages/SearchAi.jsx
+++ src/pages/SearchAi.jsx
@@ -1,7 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import { Link } from "react-router-dom";
 
 const SearchAi = () => {
+  const [searchQuery, setSetQuery] = useState("");
   const query = "I wanna watch a movie which has dark romance and some adventure, thrill";
 
   async function getSearchHandler() {
@@ -18,7 +20,12 @@ const SearchAi = () => {
     getSearchHandler();
   }, []);
 
-  return <div>SearchAi</div>;
+  return <>
+  <main className="w-full h-screen bg-zinc-950 flex flex-col items-center justify-center gap-5">
+    <h1 className="text-center text-lg md:text-xl lg:text-2xl font-bold text-red-600">Currently this feature is in Development mode</h1>
+    <Link to={"/browse"} className="bg-zinc-800 text-white text-sm font-semibold py-3 px-6 rounded-lg hover:bg-zinc-700 duration-300">Back to Homepage</Link>
+  </main>
+  </>;
 };
 
 export default SearchAi;
